2ab11ccd85aa722faf8a405fb7ae0fda7a492f86
Rafael Nazario <rafael.s.nazario@gmail.com>
2020-03-09 15:34:23 -0300

    Added exercise-4 'my_all?'

--- main.rb
+++ main.rb
@@ -42,11 +42,39 @@ def my_select (arr, last_included)
         end
         i += 1
     end
-        return aux
+    return aux
 end
 
-result = my_select( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] , true) do |x|
+exc3 = my_select( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] , true) do |x|
     x.even?
 end
 
-print result
+print exc3
+
+# exercise 4
+
+def my_all? (arr)
+    aux = []
+    i = 0
+    j = arr.length
+    while i < j do
+        if !yield(arr[i])
+            return false
+        end
+        i += 1
+    end
+    return true
+end
+
+exc4_a = my_all?(%w[ant bear cat]) do |word|
+    word.length > 3
+end
+
+exc4_b = my_all?(%w[lion bear fish]) do |word|
+    word.length > 3
+end
+
+puts exc4_a
+puts exc4_b
+
+    
