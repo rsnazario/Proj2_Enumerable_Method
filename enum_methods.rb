7ade4bf58f6988cb6915594d2019aa4f8c38e041
rsnazario <rafael.s.nazario@gmail.com>
2020-03-10 17:39:57 -0300

    Added 'my_each_with_index' to Enumerable Methods

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,12 +1,19 @@
 module Enumerable
     
   def my_each
-      return to_enum unless block_given?
-      self.length.times do
-          yield
-      end
-      
+    return to_enum unless block_given?
+    self.length.times do |i|
+        yield(self[i])
+    end
+    return self
   end
 
+  def my_each_with_index
+    return to_enum unless block_given?
+    self.length.times do |i|
+        yield(self[i], i)
+    end
+    return self
+  end
 
 end
