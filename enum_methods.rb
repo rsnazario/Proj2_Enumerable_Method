d97449bd49f8a4009495681fcb06e4ca3670b14c
rsnazario <rafael.s.nazario@gmail.com>
2020-03-11 14:50:12 -0300

    Corrected 'my_each' and 'my_each_with_index' to accept ranges

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,76 +1,80 @@
 module Enumerable
-    
-  def my_each
+  def my_each2
     return to_enum unless block_given?
-    self.length.times do |i|
-        yield(self[i])
+
+    size.times do |i|
+      yield(to_a[i])
     end
-    return self
   end
 
   def my_each_with_index
     return to_enum unless block_given?
-    self.length.times do |i|
-        yield(self[i], i)
+
+    size.times do |i|
+      yield(to_a[i], i)
     end
-    return self
   end
-  
+
   def my_select
     return to_enum unless block_given?
+
     result = []
     my_each do |i|
-        result << i if yield(i)
+      result << i if yield(i)
     end
-    return result
+    result
   end
 
   def my_all?
     return to_enum unless block_given?
+
     my_each do |i|
       return false unless yield(i)
     end
-    return true
+    true
   end
 
   def my_any?
     return to_enum unless block_given?
+
     my_each do |i|
-        return true if yield(i)
+      return true if yield(i)
     end
-    return false
+    false
   end
 
   def my_none?
     return to_enum unless block_given?
+
     my_each do |i|
-        return false if yield(i)
+      return false if yield(i)
     end
-    return true
+    true
   end
 
   def my_count(search_for = nil)
-    return self.length unless search_for != nil || block_given?
+    return length unless !search_for.nil? || block_given?
+
     count = 0
     if !block_given?
-        my_each do |i|
-            count += 1 if i == search_for
-        end
+      my_each do |i|
+        count += 1 if i == search_for
+      end
     else
-        my_each do |i|
-            count += 1 if yield(i)
-        end
+      my_each do |i|
+        count += 1 if yield(i)
+      end
     end
-    return count
+    count
   end
 
   def my_map
     return to_enum unless block_given?
+
     result = []
     my_each do |i|
       result << yield(i)
     end
-    return result
+    result
   end
-
 end
