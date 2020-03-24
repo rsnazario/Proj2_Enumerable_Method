9422ae9e4b1eafe8a97281596fe6c29f08dbce23
elbie-em <emoonga1993@gmail.com>
2020-03-24 20:01:16 +0100

    Rspec test for my_all enumerable method

--- spec/enum_methods_spec.rb
+++ spec/enum_methods_spec.rb
@@ -2,6 +2,7 @@ require './enum_methods'
 
 RSpec.describe Enumerable do
   let(:arr_1) {[1, 2, 3, 4, 5]}
+  let(:arr_2) {[1, 2, nil, 4,]}
   let(:arr_string1) {%w[cat dog wombat]}
 
   describe '#my_each' do
@@ -28,7 +29,7 @@ RSpec.describe Enumerable do
       it {expect(arr_1.my_select).to be_a(Enumerator)}
     end
     context 'if block given return array containing elements of the original array that meets condition provided' do
-      it 'returns even numbers' do 
+      it 'returns even numbers' do
         expect(arr_1.my_select{|i| i % 2  == 0}).to eq([2, 4])
       end
       it 'returns odd numbers' do 
@@ -41,8 +42,19 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_all?' do
-    
-
+    context 'if no block given return true if none of object values is nil or false' do
+      it {expect(arr_1.my_all?).to eq(true)}
+      it {expect([].my_all?).to eq(true) }
+    end
+    context 'if no block given return false if none one the object values is nil or false' do
+      it {expect(arr_2.my_all?).to eq(false)}
+    end
+    context 'if argument is Regular expression return true if all all objects contain it' do
+      it {expect(%w[ant beat cat].all?(/t/)).to eq(true)}
+    end
+    context 'if argument is Class return true if all all objects are of that class' do
+      it {expect(arr_1.all?(Integer)).to eq(true)}
+    end
   end
 
 end
